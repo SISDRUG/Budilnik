a5838951d38efdc5acba8093c74e6f477a5acfed
sisdrug <danik2004sabancev@gmail.com>
2024-12-19 17:33:13 +0300

    1.2 rebuild creation of Budilnik add Jacksone.lib for json add tests for Jacksone add budilnik.json as test json file of BudilnikRepository

---
+++ src/JacksonTest.java
@@ -0,0 +1,39 @@
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class JacksonTest {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void pojoToJsonString() throws IOException {
+        Budilnik budilnik = new Budilnik(12, 21, true);
+        BudilnikRepository budilnikRepository = new BudilnikRepository();
+        budilnikRepository.addBudilnik(budilnik);
+        budilnikRepository.addBudilnik(budilnik);
+        budilnikRepository.addBudilnik(budilnik);
+        String json = objectMapper.writeValueAsString(budilnikRepository);
+
+        System.out.println(json);
+    }
+
+    @Test
+    public void jsonFileToPojo() throws IOException {
+        File file = new File("src/budilnik.json");
+
+        BudilnikRepository budilnik = objectMapper.readValue(file, BudilnikRepository.class);
+
+        System.out.println(budilnik.getBudilniks());
+        System.out.println(budilnik.find(0).isStatus());
+        budilnik.showBudilniks();
+
+
+
+    }
+}
